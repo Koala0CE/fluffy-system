b0f6556f99cbb0668c74d91e985d7cf45029cfa0
Koala0CE <reactor.koala0c@icloud.com>
2024-06-26 13:19:02 +0200

    refactor: Add Post document type and content source configuration

---
+++ contentlayer.config.ts
@@ -0,0 +1,24 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    publishedAt: { type: "date", required: true },
+    updatedAt: { type: "date", required: true },
+    description: { type: "string", required: true },
+    image: { type: "image" },
+    isPublished: { type: "boolean", default: true },
+    tags: { type: "list", of: { type: "string" } },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: "posts", documentTypes: [Post] });
