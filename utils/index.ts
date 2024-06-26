8ccdefc6ae0f1b7ec521e51c5a4259140ad13c96
Koala0CE <reactor.koala0c@icloud.com>
2024-06-26 13:20:25 +0200

    refactor: Add utility function for sorting posts by published date

--- utils/index.ts
+++ utils/index.ts
@@ -2,5 +2,29 @@
 // It then joins the remaining class names with a space and returns the result.
 // This is useful for conditionally applying classes to an element.
 
+import { compareDesc, parseISO } from "date-fns";
+
 export const cx = (...classNames: (string | undefined | null | false)[]) =>
   classNames.filter(Boolean).join(" ");
+
+export const sortPosts = (
+  posts: {
+    image: {
+      filePath: string;
+      blurhasDataUrl: string;
+      blurDataURL: string;
+      alt: string;
+    };
+    title: string;
+    description: string;
+    tags: string[];
+    publishedAt: string;
+    url: string;
+  }[]
+) => {
+  return posts
+    .slice()
+    .sort((a: { publishedAt: string }, b: { publishedAt: string }) =>
+      compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt))
+    );
+};
