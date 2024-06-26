0d9256f492d6e92270e7cefbff3164fb36538d3c
Koala0CE <reactor.koala0c@icloud.com>
2024-06-26 13:18:10 +0200

    refactor: Add HomeCoverSection component to display posts on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import HomeCoverSection from "@/components/Home/HomeCoverSection";
+import { allPosts } from "contentlayer/generated";
 export default function Home() {
+  console.log(allPosts);
   return (
     <main className="flex flex-col items-center justify-center ">
       Hello World!
+      <HomeCoverSection posts={allPosts} />
     </main>
   );
 }
