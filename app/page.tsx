f7d4c861b5030bcd06311a98754e928be9d4bab0
Koala0CE <reactor.koala0c@icloud.com>
2024-06-26 22:13:06 +0200

    feat: Add FeaturedPosts component to display featured posts on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import FeaturedPosts from "@/components/Home/FeaturedPosts";
 import HomeCoverSection from "@/components/Home/HomeCoverSection";
 import { allPosts } from "contentlayer/generated";
 export default function Home() {
   console.log(allPosts);
   return (
     <main className="flex flex-col items-center justify-center ">
-      Hello World!
       <HomeCoverSection posts={allPosts} />
+      <FeaturedPosts posts={allPosts} />
     </main>
   );
 }
