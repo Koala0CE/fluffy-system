8ef44d390347f4fa3737c0728b3f45ce401b8d02
Koala0CE <reactor.koala0c@icloud.com>
2024-06-28 23:15:13 +0200

    refactor: Add @tailwindcss/typography plugin to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
 export default config;
