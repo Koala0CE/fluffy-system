2bf66a35780c8ef772243c8589a5aca50f106c7d
Koala0CE <reactor.koala0c@icloud.com>
2024-07-02 21:13:06 +0200

    refactor: Update Contact page layout and styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,14 +5,18 @@ import LottieAnimation from "@/components/Contact/LottieAnimation";
 
 export default function Contact() {
   return (
-    <section className="w-full h-[75vh] border-b-2 border-solid border-black flex flex-row items-center justify-center text-black">
-      <div className="inline-block w-2/5 h-full border-r-2 border-solid border-black">
-        <LottieAnimation />
-      </div>
-      <div className="w-3/5 flex flex-col items-start justify-center px-16 pb-8">
-        <h2 className="font-bold capitalize text-4xl">Hello</h2>
-        <ContactForm />
-      </div>
-    </section>
+    <div className="flex justify-center">
+      <section className="w-full max-w-[calc(100%-2rem)] h-[75vh] border-b-2 border-solid border-black flex flex-row items-center justify-center text-black mx-auto">
+        <div className="w-3/5 flex flex-col items-start justify-center px-16 pb-8">
+          <h2 className="font-bold text-4xl text-primary font-manrope">
+            Get in touch!
+          </h2>
+          <ContactForm />
+        </div>
+        <div className="inline-block w-2/5 h-full border-l-2 border-solid border-black">
+          <LottieAnimation />
+        </div>
+      </section>
+    </div>
   );
 }
