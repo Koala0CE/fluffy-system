691872b388fdac633b3486005c7882b942c02bd0
Koala0CE <reactor.koala0c@icloud.com>
2024-07-03 14:13:35 +0200

    refactor: Update PostLayoutTwo component styling to use gray text color and adjust tag font weight

--- components/Post/PostLayoutTwo.tsx
+++ components/Post/PostLayoutTwo.tsx
@@ -6,7 +6,7 @@ import { Post } from "@/utils/types";
 
 const PostLayoutTwo: React.FC<{ post: Post }> = ({ post }) => {
   return (
-    <div className="grid grid-cols-12 gap-4 items-center text-black">
+    <div className="grid grid-cols-12 gap-4 items-center text-gray-800 my-4">
       <Link
         className="col-span-4 h-full rounded-xl overflow-hidden"
         href={post.url}
@@ -22,20 +22,20 @@ const PostLayoutTwo: React.FC<{ post: Post }> = ({ post }) => {
         />
       </Link>
 
-      <div className="col-span-8 w-full">
-        <span className="uppercase text-primary font-semibold text-sm">
+      <div className="col-span-8 w-full flex flex-col">
+        <span className="uppercase text-analogous font-semibold text-sm">
           {post.tags[0]}
         </span>
 
         <Link href={post.url} className="inline-block my-1">
           <h2 className="font-semibold capitalize text-lg">
-            <span className=" text-white bg-gradient-to-r from-primary/50 to-primary/50 bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">
+            <span className=" text-gray-800 bg-gradient-to-r from-analogous/50 to-analogous/50 bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">
               {post.title}
             </span>
           </h2>
         </Link>
 
-        <span className="capitalize text-black/50 font-semibold text-base">
+        <span className="capitalize text-gray-400 font-semibold text-base">
           {format(new Date(post.publishedAt), "MMMM dd, yyyy")}
         </span>
       </div>
