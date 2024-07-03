0d28413a1d9b489d018e6f967d460128848bcceb
Koala0CE <reactor.koala0c@icloud.com>
2024-07-03 15:06:24 +0200

    refactor: Update RecentPosts component to display recent posts with correct capitalization and primary text color

--- components/Home/RecentPosts.tsx
+++ components/Home/RecentPosts.tsx
@@ -15,19 +15,19 @@ const RecentPosts: React.FC<RecentPostsProps> = ({ posts }) => {
   return (
     <section className="w-full mt-32 px-32 flex flex-col items-center justify-center">
       <div className="flex w-full justify-between">
-        <h2 className=" inline-block font-bold capitalise text-4xl ">
-          RecentPosts
+        <h2 className=" inline-block font-bold capitalise text-4xl text-primary">
+          Recent Posts
         </h2>
         <Link
           href={"/categories/all"}
-          className="inline-block font-medium text-primary underline underline-offset-2 text-lg"
+          className="inline-block font-medium text-complementary underline underline-offset-2 text-lg"
         >
           View all
         </Link>
       </div>
 
       <div className="grid grid-cols-3 grid-rows-2 gap-16 mt-16">
-        {sortedPosts.slice(5, 11).map((post, index) => {
+        {sortedPosts.slice(0, 6).map((post, index) => {
           return (
             <article className="col-span-1 row-span-1 relative ">
               <PostLayoutThree post={post} />
