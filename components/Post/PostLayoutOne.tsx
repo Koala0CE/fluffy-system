18b8fb22fba5dfe0b3c8a75bbacd9272c0387d6b
Koala0CE <reactor.koala0c@icloud.com>
2024-07-03 14:13:40 +0200

    refactor: Update PostLayoutOne component styling for image overlay and tag styling

--- components/Post/PostLayoutOne.tsx
+++ components/Post/PostLayoutOne.tsx
@@ -7,8 +7,8 @@ import { slug } from "github-slugger";
 
 const PostLayoutOne: React.FC<{ post: Post }> = ({ post }) => {
   return (
-    <div className="inline-block overflow-hidden rounded-x1">
-      <div className="absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-black/90 rounded-3xl z-10"></div>
+    <div className="inline-block overflow-hidden rounded-x1 relative">
+      <div className="absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent to-black/90 rounded-3xl z-10"></div>
       <Image
         src={post.image.filePath.replace("../public", "")}
         alt={post.title}
@@ -19,14 +19,14 @@ const PostLayoutOne: React.FC<{ post: Post }> = ({ post }) => {
 
       <div className="absolute bottom-0 p-10 w-full z-20">
         <Tag
-          className="px-6 text-sm py-2 !border"
+          className="bg-primary hover:bg-pink-600 capitalize text-white font-semibold py-2 px-4 border border-complementary rounded shadow"
           link={`/categories/${slug(post.tags[0])}`}
           name={post.tags[0]}
         />
 
-        <Link href={post.url} className="mt-6">
+        <Link href={post.url} className="block mt-4">
           <h2 className="font-bold capitalize text-2xl">
-            <span className="mt-4 text-white bg-gradient-to-r from-primary to-primary bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">
+            <span className="text-white bg-gradient-to-r from-primary to-primary bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">
               {post.title}
             </span>
           </h2>
