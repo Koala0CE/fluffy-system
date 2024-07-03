55780e96f22af0f55005f5004d4287afec6c29a9
Koala0CE <reactor.koala0c@icloud.com>
2024-07-03 14:13:48 +0200

    refactor: Update FeaturedPosts component to use correct capitalization and primary text color

--- components/Home/FeaturedPosts.tsx
+++ components/Home/FeaturedPosts.tsx
@@ -13,8 +13,8 @@ const FeaturedPosts: React.FC<FeaturedPostsProps> = ({ posts }) => {
 
   return (
     <section className="w-full mt-32 px-32 flex flex-col items-center justify-center">
-      <h2 className="w-full inline-block font-bold capitalise text-4xl ">
-        FeaturedPosts
+      <h2 className="w-full inline-block font-bold capitalise text-4xl text-primary">
+        Featured Posts
       </h2>
       <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-16">
         <article className="col-span-1 row-span-2 relative">
