980c1f24f7821f3ff3795f0cf10dd4f4b736d38b
Koala0CE <reactor.koala0c@icloud.com>
2024-07-04 15:44:42 +0200

    refactor: Add Table of Contents (TOC) to Post page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -47,7 +47,37 @@ export default function Page({ params }: { params: { slug: string } }) {
       <PostDetails post={post} slug={params.slug} />
 
       <div className="grid grid-cols-12 gap-5 mt-8 px-10">
-        <div className="col-span-4 bg-gray-100">Toc</div>
+        <div className="col-span-4">
+          <details
+            className="border-[1px] border-solid border-black text-black rounded-lg p-4 sticky top-6 max-h[80vh] overflow-hidden overlfow-y-auto"
+            open
+          >
+            <summary className="text-lg font-semibold capitalize cursor=pointer">
+              Table of Contents
+            </summary>
+            <ul className="mt-4 font-inter text-base">
+              {post.toc.map((heading: any) => (
+                <li className="py-1" key={heading.slug}>
+                  <a
+                    href={`#${heading.slug}`}
+                    data-level={heading.level}
+                    className="data-[level=two]:pl-0
+                    data-[level=two]:pt-2 data-[level=two]:border-t border-solid border-black/40
+                    
+                    data-[level=three]:pl-6 flex items-center justify-start"
+                  >
+                    {heading.level === "three" ? (
+                      <span className="flex w-1 h-1 rounded-full bg-black mr-2">
+                        &nbsp;
+                      </span>
+                    ) : null}
+                    <span className="hover:underline">{heading.text}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </details>
+        </div>
         <RenderMdx post={post} />
       </div>
     </article>
