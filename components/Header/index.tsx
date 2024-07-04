03be437b87238bff27260e8b309d2c2992ed1b30
Koala0CE <reactor.koala0c@icloud.com>
2024-07-04 13:55:59 +0200

    refactor: Update Header component to add scroll functionality and improve navigation styling

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,26 +1,98 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Logo from "./Logo";
 import Link from "next/link";
 import { GitHubIcon } from "@/Icons";
+import {
+  FaHome,
+  FaInfoCircle,
+  FaProjectDiagram,
+  FaEnvelope,
+} from "react-icons/fa";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // className="w-max py-1 px-8 border border-solid border-blue-900 text-white  font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-gray-800 backdrop-blur-sm z-50">
+
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
       <Logo />
-      <nav className="w-max py-3 px-8 border border-solid border-primary rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-complementary/80 backdrop-blur-sm z-50">
-        <Link href="/" className="mr-2">
-          Home
-        </Link>
-        <Link href="/about" className="mr-2">
-          About
-        </Link>
-        <Link href="/projects" className="mr-2">
-          Projects
-        </Link>
-        <Link href="/contact" className="mx-2">
-          Contact
-        </Link>
-        <button></button>
+      <nav
+        className={`${
+          isScrolled
+            ? "fixed top-0 right-0 h-full w-10 flex flex-col items-center justify-center py-10 "
+            : "w-max py-1 px-8  border-blue-900 text-white flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-gray-800 backdrop-blur-sm"
+        } text-white font-medium capitalize z-50 transition-all duration-300 ease-in-out`}
+      >
+        <div
+          className={`${
+            isScrolled
+              ? "bg-gray-50 flex flex-col py-10 px-1 text-gray-800"
+              : ""
+          } `}
+        >
+          {isScrolled ? (
+            <>
+              <Link href="/" className="mb-3 hover:text-complementary">
+                <FaHome className="w-6 h-6" />
+              </Link>
+              <Link href="/about" className="mb-3 hover:text-complementary">
+                <FaInfoCircle className="w-6 h-6" />
+              </Link>
+              <Link href="/projects" className="mb-3 hover:text-complementary">
+                <FaProjectDiagram className="w-6 h-6" />
+              </Link>
+              <Link href="/contact" className="mb-3 hover:text-complementary">
+                <FaEnvelope className="w-6 h-6" />
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                href="/"
+                className="mr-2 hover:text-complementary hover:italic"
+              >
+                Home
+              </Link>
+              <Link
+                href="/about"
+                className="mr-2 hover:text-complementary hover:italic"
+              >
+                About
+              </Link>
+              <Link
+                href="/projects"
+                className="mr-2 hover:text-complementary hover:italic"
+              >
+                Projects
+              </Link>
+              <Link
+                href="/contact"
+                className="mx-2 hover:text-complementary hover:italic"
+              >
+                Contact
+              </Link>
+              <button></button>
+            </>
+          )}
+        </div>
       </nav>
       <div>
         <a
