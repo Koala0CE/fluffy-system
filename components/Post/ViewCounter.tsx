87b1429d578410279ac21bb09690a69d9ebb9a15
Koala0CE <reactor.koala0c@icloud.com>
2024-07-09 17:08:43 +0200

    refactor: Update ViewCounter component to use correct import path for createClient

--- components/Post/ViewCounter.tsx
+++ components/Post/ViewCounter.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { createClient } from "@/utils/supabase/client";
-import React, { useState, useEffect } from "react";
-import 
 
-const supabase = createClientComponentClient();
+import { createClient } from "@/app/utils/supabase/client";
+import React, { useState, useEffect } from "react";
+
+
+interface ViewCount {
+  count: number;
+
+}
 
 interface ViewCounterProps {
   slug: string;
@@ -18,9 +22,11 @@ const ViewCounter: React.FC<ViewCounterProps> = ({
   showCount = true,
 }) => {
   const [views, setViews] = useState<number>(0);
+  const supabase = createClient();
 
   useEffect(() => {
     const incrementView = async () => {
+      console.log("Incrementing view count"); // Log when incrementing
       try {
         let { error } = await supabase.rpc("increment", {
           slug_text: slug,
@@ -36,6 +42,33 @@ const ViewCounter: React.FC<ViewCounterProps> = ({
     };
     if (!noCount) incrementView();
   }, [slug, noCount]);
+
+
+  useEffect(() => {
+    const getViews = async () => {
+      console.log("Fetching view count"); // Log when fetching view count
+      try {
+    
+let { data,  error } = await supabase
+.from('views')
+.select('count').match({slug: slug}).single()
+
+        if (error)
+          console.error(
+            "Error incrementing view count inside try block",
+            error
+          );
+
+       setViews(data ? data.count : 0  )
+
+     
+      } catch (error) {
+        console.error("Error incrementing view count", error);
+      }
+    };
+ getViews();
+  }, [slug]);
+
 
   if (showCount) {
     return <span>{views} views</span>;
