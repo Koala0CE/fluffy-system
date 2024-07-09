41dffb08ee28112d530e685fe76d798e9a931453
Koala0CE <reactor.koala0c@icloud.com>
2024-07-09 20:23:52 +0200

    refactor: Update page metadata generation to include Open Graph and Twitter card information

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,13 @@
 import AboutCoverSection from "@/components/About/AboutCoverSection";
 import Skills from "@/components/About/Skills";
+import { siteMetaData } from "@/utils/siteMetaData";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "About Me",
+  description: `Learn more about me and my skills! ${siteMetaData.email}`,
+};
 
 export default function About() {
   return (
@@ -8,8 +15,8 @@ export default function About() {
       <AboutCoverSection />
       <Skills />
       <h2 className="mt-8 font-semibold text-2xl self-start mx-20 text-gray-800">
-        If you'd like to collaborate or have any questions, don't hesitate to{" "}
-        {""}
+        If you&apos;d like to collaborate or have any questions, don&apos;t
+        hesitate to {""}
         <Link
           className="!underline underline-offset-2 text-teal-500 hover:text-teal-600"
           href={"/contact"}
