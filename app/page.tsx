4015793b2df4e45853e52c2c6b823a073bfc676d
Koala0CE <reactor.koala0c@icloud.com>
2024-07-10 12:22:54 +0200

    refactor: Remove unused console.log in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import FeaturedPosts from "@/components/Home/FeaturedPosts";
 import HomeCoverSection from "@/components/Home/HomeCoverSection";
 import RecentPosts from "@/components/Home/RecentPosts";
 import { allPosts } from "contentlayer/generated";
+
 export default function Home() {
-  console.log(allPosts);
   return (
     <main className="flex flex-col items-center justify-center ">
       <HomeCoverSection posts={allPosts} />
