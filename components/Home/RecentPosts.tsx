42da846237af0d28ec3d831cf8d49a4bc663a39c
Koala0CE <reactor.koala0c@icloud.com>
2024-07-11 17:01:24 +0200

    refactor: Update RecentPosts component layout and styling

--- components/Home/RecentPosts.tsx
+++ components/Home/RecentPosts.tsx
@@ -9,21 +9,21 @@ const RecentPosts: React.FC<{ posts: Post[] }> = ({ posts }) => {
   const sortedPosts = sortPosts(posts);
 
   return (
-    <section className="w-full mt-32 px-32 flex flex-col items-center justify-center">
+    <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
       <div className="flex w-full justify-between">
-        <h2 className=" inline-block font-bold capitalise text-4xl text-primary">
+        <h2 className="w-fit inline-block font-bold capitalize text-2xl md:text-4xl text-primary dark:text-triadic">
           Recent Posts
         </h2>
         <Link
           href={"/categories/all"}
-          className="inline-block font-medium text-complementary underline underline-offset-2 text-lg"
+          className="inline-block font-medium text-complementary dark:text-compplementaryy underline underline-offset-2 text-base md:text-lg"
         >
           View all
         </Link>
       </div>
 
       <div
-        className={`grid grid-cols-3 gap-16 mt-16 ${
+        className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 mt-16 ${
           sortedPosts.length <= 3 ? "grid-rows-1" : "grid-rows-2"
         }`}
       >
