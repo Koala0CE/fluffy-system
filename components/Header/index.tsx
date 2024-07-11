916574c1241a497b048db4760d318abf4d943f3a
Koala0CE <reactor.koala0c@icloud.com>
2024-07-11 15:46:44 +0200

    feat: Add mobile menu toggle functionality to Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -15,6 +15,7 @@ import { useThemeSwitch } from "../Hooks/useThemeSwitch";
 const Header = () => {
   const [mode, setMode] = useThemeSwitch();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [click, setClick] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,16 +33,117 @@ const Header = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setClick(!click);
+  };
+
   // className="w-max py-1 px-8 border border-solid border-blue-900 text-white  font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-gray-800 backdrop-blur-sm z-50">
 
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
       <Logo />
+
+      <button className="inline-block sm:hidden z-50" onClick={toggleMenu}>
+        <div className="w-6 cursor-pointer transition-all ease duration-300">
+          <div className="relative">
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-black dark:bg-white rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(-45deg) translateY(0)"
+                  : "rotate(0) translateY(6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-black dark:bg-white rounded transition-all ease duration-200"
+              style={{
+                opacity: click ? 0 : 1,
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-black dark:bg-white rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(45deg) translateY(0)"
+                  : "rotate(0) translateY(-6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+          </div>
+        </div>
+      </button>
+
+      <nav
+        className="w-max py-3 px-6 sm:px-8 sm:px-10 border-blue-900 text-white sm:hidden flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-gray-800 backdrop-blur-sm transition-all ease duration-300"
+        style={{
+          top: click ? "1rem" : "-5rem",
+        }}
+      >
+        <Link href="/" className="mr-2 hover:text-complementary hover:italic">
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="mr-2 hover:text-complementary hover:italic"
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className="mx-2 hover:text-complementary hover:italic"
+        >
+          Contact
+        </Link>
+        <button
+          className="inline-block ml-1"
+          onClick={() => {
+            setMode(mode === "dark" ? "light" : "dark");
+          }}
+        >
+          {mode === "dark" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              />
+            </svg>
+          )}
+        </button>
+      </nav>
+
       <nav
         className={`${
           isScrolled
             ? "fixed top-0 right-0 h-full w-10 flex flex-col items-center justify-center py-10 "
-            : "w-max py-1 px-8  border-blue-900 text-white flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-gray-800 backdrop-blur-sm"
+            : "w-max py-1 px-5 sm:px-10  border-blue-900 text-white hidden sm:flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-gray-800 backdrop-blur-sm"
         } text-white font-medium capitalize z-50 transition-all duration-300 ease-in-out`}
       >
         <div
@@ -134,7 +236,7 @@ const Header = () => {
           )}
         </div>
       </nav>
-      <div>
+      <div className="hidden sm:flex items-center">
         <a
           href={siteMetaData.linkedin}
           target="_blank"
@@ -149,7 +251,7 @@ const Header = () => {
           rel="noopener noreferrer"
           className="inline-block w-6 h-6 mr-4"
         >
-          <GitHubIcon className="hover:scale-125 transition-all ease duration-200" />
+          <GitHubIcon className="hover:scale-125 transition-all ease duration-200 dark:fill-white" />
         </a>
       </div>
     </header>
