fb909c4c23c4c43c02f3e595736e953c4e288bd6
Koala0CE <reactor.koala0c@icloud.com>
2024-07-11 15:46:14 +0200

    refactor: Update Logo component to improve accessibility and styling

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -5,15 +5,17 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <Link href={"/"} className="flex items-center text-dark">
-      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
+    <Link href={"/"} className="flex items-center text-black dark:text-white">
+      <div className="w-12 md:w-16 rounded-full overflow-hidden border border-solid border-primary mr-2 md:mr-4 dark:border-primary">
         <Image
           src={logo}
           alt="Elvy Logo"
           className="w-full h-auto rounded-full"
         />
       </div>
-      <span className=" text-xl font-bold ml-2">ElvyCrissy</span>
+      <span className="text-lg md:text-xl font-bold dark:font-semibold ml-2 text-primary dark:text-primary">
+        ElvyCrissy
+      </span>
     </Link>
   );
 };
