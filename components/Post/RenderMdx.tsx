b470b0cd1782d4c87f4fa4a16bbbe419a12912c5
Koala0CE <reactor.koala0c@icloud.com>
2024-07-12 17:38:48 +0200

    refactor: Update RenderMdx component styling for dark mode and responsiveness

--- components/Post/RenderMdx.tsx
+++ components/Post/RenderMdx.tsx
@@ -30,7 +30,7 @@ const mdxComponents = {
 const RenderMdx = ({ post }: any) => {
   const MDXContent = useMDXComponent(post.body.code);
   return (
-    <div className="text-gray-800 col-span-8 font-inter prose-lg max-w-max prose-blockquote:bg-purple/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-purple prose-blockquuote:not-italic prose-blockquote:rounded-r-lg prose-li:marker-purple">
+    <div className="text-gray-800 dark:text-gray-200 col-span-12 lg:col-span-8 font-inter prose sm:prose-base md:prose-lg max-w-max prose-blockquote:bg-complementary/20 prose-blockquote:bg-triadic/20prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-purple-200 dark:prose-blockquote:border-triadic prose-blockquote:not-italic prose-blockquote:rounded-r-lg prose-li:marker-complementary dark:prose-li:marker-red-100 dark:prose-invert first-letter:text-3xl sm:first-letter:text-5xl">
       <MDXContent components={mdxComponents} />
     </div>
   );
