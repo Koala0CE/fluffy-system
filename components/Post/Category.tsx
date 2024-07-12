df844f80ff227f940d8fe18feabb9341b7632a10
Koala0CE <reactor.koala0c@icloud.com>
2024-07-12 17:38:42 +0200

    refactor: Update Category component to accept additional props and improve code readability

--- components/Post/Category.tsx
+++ components/Post/Category.tsx
@@ -2,17 +2,32 @@ import { cx } from "@/utils";
 import Link from "next/link";
 import React from "react";
 
-const Category = ({ link = "#", name, active, ...props }: any) => {
+interface CategoryProps {
+  link?: string;
+  name: string;
+  active?: boolean;
+  className?: string;
+  [key: string]: any;
+}
+
+const Category: React.FC<CategoryProps> = ({
+  link = "#",
+  name,
+  active,
+  className,
+  ...props
+}) => {
   return (
     <Link
       className={cx(
-        "inline-block py-1.5 md:py-2 px-6  md:px-10 rounded-full border-2 border-solid border-black dark:border-white hover:scale-105 transition-all ease duration-200 m-2",
-        props.className,
+        "inline-block py-1.5 md:py-2 px-6 md:px-10 rounded-full border-2 border-solid border-black dark:border-white hover:scale-105 transition-all ease duration-200 m-2",
+        className,
         active
           ? "bg-black text-white dark:bg-white dark:text-black"
           : "bg-orange-500 text-black dark:bg-black dark:text-white"
       )}
       href={link}
+      {...props}
     >
       #{name}
     </Link>
