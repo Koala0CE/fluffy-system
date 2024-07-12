e5de7014fd2550b15363de1e6f06e04bb8448c4d
Koala0CE <reactor.koala0c@icloud.com>
2024-07-12 17:38:02 +0200

    refactor: Update Category component styling for dark mode and responsiveness

--- components/Post/Category.tsx
+++ components/Post/Category.tsx
@@ -6,12 +6,13 @@ const Category = ({ link = "#", name, active, ...props }: any) => {
   return (
     <Link
       className={cx(
-        "inline-block py-2 px-10 rounded-full font-normal border-2 border-solid border-black hover:scale-105 transition-all ease duration-200 m-2",
+        "inline-block py-1.5 md:py-2 px-6  md:px-10 rounded-full border-2 border-solid border-black dark:border-white hover:scale-105 transition-all ease duration-200 m-2",
         props.className,
-        active ? "bg-black text-white" : "bg-orange-500 text-black"
+        active
+          ? "bg-black text-white dark:bg-white dark:text-black"
+          : "bg-orange-500 text-black dark:bg-black dark:text-white"
       )}
       href={link}
-      {...props}
     >
       #{name}
     </Link>
