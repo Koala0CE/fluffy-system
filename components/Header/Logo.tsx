eda0aea131b6a70088354ea463e3ee6511a2c3f9
Koala0CE <reactor.koala0c@icloud.com>
2024-07-14 11:05:39 +0200

    refactor: Update Logo component to include lazy loading and responsive image sizes

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -9,8 +9,10 @@ const Logo = () => {
       <div className="w-12 md:w-16 rounded-full overflow-hidden border border-solid border-primary mr-2 md:mr-4 dark:border-primary">
         <Image
           src={logo}
-          alt="Elvy Logo"
+          alt="Koala0CE Logo"
           className="w-full h-auto rounded-full"
+          sizes="33w"
+          priority
         />
       </div>
       <span className="text-lg md:text-xl font-bold dark:font-semibold ml-2 text-primary dark:text-primary">
