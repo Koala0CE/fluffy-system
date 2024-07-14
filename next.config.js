d24fd38430aa50b545ead79ee54240a0c0827437
Koala0CE <reactor.koala0c@icloud.com>
2024-07-14 11:05:48 +0200

    refactor: Add compiler option to remove console logs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { withContentlayer } = require("next-contentlayer2");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: { removeConsole: true },
+};
 
 module.exports = withContentlayer(nextConfig);
