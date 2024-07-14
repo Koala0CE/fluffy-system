cd61c84c6bc702078c7ddaac05b052ac14a10b89
Koala0CE <reactor.koala0c@icloud.com>
2024-07-14 13:29:43 +0200

    refactor: Optimize image loading and responsiveness in Page component

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -37,9 +37,7 @@ export async function generateMetadata({
   }
 
   const ogImages = imageList.map((image) => ({
-    return: {
-      url: image.includes("http") ? image : siteMetaData.siteUrl + image,
-    },
+    url: image.includes("http") ? image : siteMetaData.siteUrl + image,
   }));
 
   const authors = post?.author ? [post.author] : [siteMetaData.author];
@@ -77,66 +75,100 @@ export default function Page({ params }: { params: { slug: string } }) {
     return <div>Post or tags not found.</div>;
   }
 
+  let imageList = [siteMetaData.socialBanner];
+  if (post && post.image) {
+    imageList =
+      typeof post.image.filePath === "string"
+        ? [siteMetaData.siteUrl + post.image.filePath.replace("../public", "")]
+        : [post.image.filePath];
+  }
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    headline: post?.title,
+    description: post?.description,
+    image: imageList,
+    datePublished: new Date(post.publishedAt).toISOString(),
+    dateModified: new Date(post.updatedAt || post.publishedAt).toISOString(),
+    author: {
+      "@type": "Person",
+      name: post?.author || siteMetaData.author,
+      url: siteMetaData.siteUrl + post.url, // replace with profile URL of the author
+    },
+  };
+
   return (
-    <article>
-      <div className="mb-8 text-center relative w-full h-[70vh] bg-black">
-        <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Tag
-            className="px-6 text-sm py-2"
-            name={post.tags[0]}
-            link={`/categories/${slug(post.tags[0])}`}
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+
+      <article>
+        <div className="mb-8 text-center relative w-full h-[70vh] bg-black">
+          <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Tag
+              className="px-6 text-sm py-2"
+              name={post.tags[0]}
+              link={`/categories/${slug(post.tags[0])}`}
+            />
+
+            <h1 className="inline-block mt-5 font-semibold capitalize text-white text-2xl md:text-3xl lg:text-5xl leading-normal relative w-5/6">
+              {post.title}
+            </h1>
+          </div>
+
+          <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-black/60 dark:bg-black/40" />
+          <Image
+            src={post.image.filePath.replace("../public", "")}
+            alt={post.title}
+            width={post.image.width}
+            height={post.image.height}
+            placeholder="blur"
+            blurDataURL={post.image.blurhashDataUrl}
+            className="aspect-square w-full h-full object-cover object-center"
+            priority
+            sizes="100vw"
           />
-
-          <h1 className="inline-block mt-5 font-semibold captitalize text-white text-2xl md:text-3xl lg:text-5xl leading-normal relative w-5/6"></h1>
         </div>
+        <PostDetails post={post} slug={params.slug} />
 
-        <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-black/60 dark:bg-black/40" />
-        <Image
-          src={post.image.filePath.replace("../public", "")}
-          alt={post.title}
-          width={post.image.width}
-          height={post.image.height}
-          placeholder="blur"
-          blurDataURL={post.image.blurhashDataUrl}
-          className="aspect-square w-full h-full object-cover object-center"
-        />
-      </div>
-      <PostDetails post={post} slug={params.slug} />
-
-      <div className="grid grid-cols-12 gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
-        <div className="col-span-12 lg:col-span-4">
-          <details
-            className="border-[1px] border-solid border-black dark:border-white text-black dark:text-white rounded-lg p-4 sticky top-6 max-h[80vh] overflow-hidden overlfow-y-auto"
-            open
-          >
-            <summary className="text-lg font-semibold capitalize cursor=pointer">
-              Table of Contents
-            </summary>
-            <ul className="mt-4 font-inter text-base">
-              {post.toc.map((heading: any) => (
-                <li className="py-1" key={heading.slug}>
-                  <a
-                    href={`#${heading.slug}`}
-                    data-level={heading.level}
-                    className="data-[level=two]:pl-0
-                    data-[level=two]:pt-2 data-[level=two]:border-t border-solid border-black/40
-                    data-[level=three]:pl-4
-                    sm:data-[level=three]:pl-6 flex items-center justify-start"
-                  >
-                    {heading.level === "three" ? (
-                      <span className="flex w-1 h-1 rounded-full bg-black mr-2">
-                        &nbsp;
-                      </span>
-                    ) : null}
-                    <span className="hover:underline">{heading.text}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </details>
+        <div className="grid grid-cols-12 gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
+          <div className="col-span-12 lg:col-span-4">
+            <details
+              className="border-[1px] border-solid border-black dark:border-white text-black dark:text-white rounded-lg p-4 sticky top-6 max-h[80vh] overflow-hidden overflow-y-auto"
+              open
+            >
+              <summary className="text-lg font-semibold capitalize cursor-pointer">
+                Table of Contents
+              </summary>
+              <ul className="mt-4 font-inter text-base">
+                {post.toc.map((heading: any) => (
+                  <li className="py-1" key={heading.slug}>
+                    <a
+                      href={`#${heading.slug}`}
+                      data-level={heading.level}
+                      className="data-[level=two]:pl-0
+                      data-[level=two]:pt-2 data-[level=two]:border-t border-solid border-black/40
+                      data-[level=three]:pl-4
+                      sm:data-[level=three]:pl-6 flex items-center justify-start"
+                    >
+                      {heading.level === "three" ? (
+                        <span className="flex w-1 h-1 rounded-full bg-black mr-2">
+                          &nbsp;
+                        </span>
+                      ) : null}
+                      <span className="hover:underline">{heading.text}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          </div>
+          <RenderMdx post={post} />
         </div>
-        <RenderMdx post={post} />
-      </div>
-    </article>
+      </article>
+    </>
   );
 }
